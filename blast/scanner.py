3081e1af4437721dca8e5d570d16be4573ea4abd
Vini Salazar <viniws@gmail.com>
2018-12-11 11:49:40 -0200

    Added parentheses to all print statements

--- blast/scanner.py
+++ blast/scanner.py
@@ -12,10 +12,10 @@ class DirScanner(object):
         self.absolute = absolute
         self.stripdot = stripdot
         return
-    
+
     def md5sum(self, f):
         ''' md5sums a file, returning the hex digest
-            
+
             Parameters:
                 - f     filename string
         '''
@@ -27,10 +27,10 @@ class DirScanner(object):
             m.update(chunk)
         fh.close()
         return m.hexdigest()
-    
+
     def iteritems(self, path=None, want_files=True, want_dirs=True, func=None, filt=None):
         ''' streaming item iterator that can optionally run a function / filter function on each item (files/dirs)
-            
+
             Parameters:
                 - path          path to iterate on (note: this is called recursively)
                 - want_files    True if you want file results in the iteration, False if you don't
@@ -40,26 +40,26 @@ class DirScanner(object):
         '''
         if path is None: iter_path = self.path
         else:            iter_path = path
-        
+
         for f in os.listdir(iter_path):
             if f[0] == '.': continue
-            
+
             ## f (filename) -> af (absolute filename)
             if self.absolute: af = os.path.abspath( os.path.join(iter_path, f) )
             else:             af = os.path.join(iter_path, f)
-            
+
             ## filter out stuff we don't want
             if filt and not filt(af): continue
 
             ## detect broken path strings
             if not os.path.exists(af): raise IOError('bad path: %s' % af)
-            
+
             ## return our main response
             if ( os.path.isfile(af) and want_files ) or ( os.path.isdir(af) and want_dirs ):
                 if self.stripdot and af[:2] == './': af = af[2:]
                 if func: yield ( func(af), af )
                 else:    yield af
-            
+
             ## recurse & return for sub-dirs
             if os.path.isdir(af):
                 for x in self.iteritems(path       = af,
@@ -67,7 +67,7 @@ class DirScanner(object):
                                         want_dirs  = want_dirs,
                                         func       = func,
                                         filt       = filt): yield x
-    
+
     def iterdupes(self, compare=None, filt=None):
         ''' streaming item iterator with low overhead duplicate file detection
 
@@ -88,8 +88,8 @@ class DirScanner(object):
                     if chksum in seen_sum:  yield (chksum, seen_siz[fsize])
                     else:                   seen_sum[chksum] = seen_siz[fsize]
                     seen_siz[fsize] = None
-                
-                ## state 3: defined key => None (already scanned path, no-op) 
+
+                ## state 3: defined key => None (already scanned path, no-op)
                 chksum = compare(f)
                 if chksum in seen_sum:
                     ## if it's a dupe, check if the first one was ever yielded then yield
@@ -104,4 +104,4 @@ class DirScanner(object):
 
 if __name__ == '__main__':
     ls = DirScanner()
-    for item in ls.iteritems(): print item
+    for item in ls.iteritems(): print(item)
